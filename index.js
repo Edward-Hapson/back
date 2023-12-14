b73ee989599583dac6efc27570561241e619e5d9
Edward-Hapson <111171926+Edward-Hapson@users.noreply.github.com>
2023-12-14 12:06:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const cors = require ('cors')
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
+const connectDB = async()=> {
+    const connect = await mongoose.connect('mongodb://localhost:27017')
+    if(connect){
+        console.log('database connected');
+    }
+}
+connectDB();
 
 app.get('/api', (req,res)=> {
-    res.status(200).send('hello, there')
+        res.status(200).json({message: 'hello there'})
+
 })
 
-app.listen(4000, ()=> {
-    console.log('server connected on port 4000')
+app.listen(6262, ()=> {
+    console.log('server connected on port 6262')
 })
